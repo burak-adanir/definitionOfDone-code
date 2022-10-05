ac5ed4757d619c799192cef811433299edb19baf
burak-adanir <112620862+burak-adanir@users.noreply.github.com>
2022-10-05 16:23:28 +0200

    Create main.c

---
+++ main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include "source.h"
+void main()
+{   int x;
+    int meineZahl = 0;printf ("How many numbers do you want");scanf ("%d", &meineZahl);meineZahl = 0;
+
+    for (x = 1; x< deineZahl(); x++)
+    {
+        printf("%d\t", x);
+    }
+}
